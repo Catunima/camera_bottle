8ffb468d45bca28ac410f6c6d1474aca2a7eff0c
Catunima <2009055@upy.edu.mx>
2023-12-08 15:03:42 -0600

    correccion conteo

--- model.py
+++ model.py
@@ -4,30 +4,52 @@ import numpy as np
 
 # Leemos el modelo
 model = torch.hub.load('ultralytics/yolov5', 'custom',
-                       path = r'D:\Projects\Artificial_vision\camara_detector_robot\best.pt')
+                       path=r'D:\Projects\Artificial_vision\camara_detector_robot\best.pt')
 
 # Realizo Videocaptura
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
+
+# Inicializamos el contador
+contador_botellas = 0
+
+# Inicializamos un conjunto para llevar un registro de las botellas ya contadas
+botellas_contadas = set()
 
 # Empezamos
 while True:
     # Realizamos lectura de frames
     ret, frame = cap.read()
 
-    # Correccion de color
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
     # Realizamos las detecciones
     detect = model(frame)
 
     info = detect.pandas().xyxy[0]  # im1 predictions
-    print(info)
 
-    # Mostramos FPS
-    cv2.imshow('Detector de Carros', np.squeeze(detect.render()))
+    # Iteramos sobre las detecciones
+    for index, row in info.iterrows():
+        botella_id = index  # Puedes usar otra información para identificar la botella si es necesario
+
+        # Verificamos si esta botella ya se ha contado
+        if botella_id not in botellas_contadas:
+            # Incrementamos el contador
+            contador_botellas += 1
+
+            # Agregamos esta botella al conjunto de botellas contadas
+            botellas_contadas.add(botella_id)
+
+    # Dibujamos el número de botellas detectadas en el frame
+    cv2.putText(frame, f'Botellas: {contador_botellas}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+    # Dibujamos las cajas delimitadoras de las botellas
+    for index, row in info.iterrows():
+        x_min, y_min, x_max, y_max = map(int, [row['xmin'], row['ymin'], row['xmax'], row['ymax']])
+        cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+
+    # Mostramos FPS y resultado
+    cv2.imshow('Detector de botellas', frame)
 
     # Leemos el teclado
-    t = cv2.waitKey(5)
+    t = cv2.waitKey(1)
     if t == 27:
         break
 
